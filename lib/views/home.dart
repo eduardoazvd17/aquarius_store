fc81ea28852f25bed47571b3a5706290968f16ae
Eduardo Azevedo <eduardoazevedo@MacBook-Pro-de-Eduardo.local>
2020-06-25 17:19:22 -0300

    Centralização de titulo

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Início'),
+      ),
+    );
   }
 }
