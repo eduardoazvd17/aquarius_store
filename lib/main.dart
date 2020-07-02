b1c46ad27b448941b3f7a276f06964b364cae965
Eduardo Azevedo <eduardoazvd17@gmail.com>
2020-07-02 10:47:01 -0300

    Removendo o get_storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:aquariusstore/views/nav_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 
-void main() async {
-  await GetStorage.init();
+void main() {
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
