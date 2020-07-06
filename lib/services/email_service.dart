2dd2e84570694da4e8bdb9c7cf8b7c6d5b3d51bd
Eduardo Azevedo Regueira <eduardoazvd17@gmail.com>
2020-07-06 14:57:56 -0300

    Removendo dados de login do smtp

--- lib/services/email_service.dart
+++ lib/services/email_service.dart
@@ -1,17 +1,15 @@
+import 'package:aquariusstore/utils/smtp_login_info.dart';
 import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server/gmail.dart';
 
 class EmailService {
-  String _username = 'noreply.aquariusstore@gmail.com';
-  String _password = 'password';
-
   forgotPass(String userEmail, String randomCode) async {
     String title = 'Aquarius Store - Recuperação de senha';
     String content =
         'Olá, se você solicitou a recuperação de senha este é o seu codigo: $randomCode.\n\nInsira-o no app para redefinir sua senha. Caso não tenha solicitado, apenas ignore este e-mail.\n\nAtenciosamente, Aquarius Store.';
-    final smtpServer = gmail(_username, _password);
+    final smtpServer = gmail(SmtpLoginInfo.USERNAME, SmtpLoginInfo.PASSWORD);
     final message = Message()
-      ..from = Address(_username, 'Aquarius Store')
+      ..from = Address(SmtpLoginInfo.USERNAME, 'Aquarius Store')
       ..recipients.add(userEmail)
       ..subject = title
       ..text = content;
