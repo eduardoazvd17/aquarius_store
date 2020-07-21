52a559998046ccbe40d25a0067318d92a7ef36b6
Eduardo Azevedo <eduardoazvd17@gmail.com>
2020-07-20 22:14:28 -0300

    Removendo imports inutilizados

--- lib/views/add_product.dart
+++ lib/views/add_product.dart
@@ -1,4 +1,3 @@
-import 'package:aquariusstore/controllers/product_controller.dart';
 import 'package:aquariusstore/models/product.dart';
 import 'package:aquariusstore/services/product_service.dart';
 import 'package:flutter/material.dart';
