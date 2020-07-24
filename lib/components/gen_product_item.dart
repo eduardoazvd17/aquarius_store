4cece4579313637130d43faecf22a6bfc8f3ffd1
Eduardo Azevedo <eduardoazvd17@gmail.com>
2020-07-24 11:44:56 -0300

    Alterando listtile para expansiontile

--- lib/components/gen_product_item.dart
+++ lib/components/gen_product_item.dart
@@ -13,9 +13,7 @@ class GenProductItem extends StatelessWidget {
   GenProductItem(this.product);
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {},
-      isThreeLine: true,
+    return ExpansionTile(
       leading: Container(
         width: 50,
         height: 50,
@@ -32,63 +30,64 @@ class GenProductItem extends StatelessWidget {
                 ),
         ),
       ),
+      children: <Widget>[
+        ListTile(
+          onTap: () => Get.to(AddPhotos(product: product)),
+          leading: Icon(Icons.camera_alt),
+          title: Text('Gerenciar Fotos'),
+        ),
+        ListTile(
+          onTap: () => Get.to(
+            AddProduct(product: this.product),
+          ),
+          leading: Icon(Icons.edit),
+          title: Text('Editar'),
+        ),
+        ListTile(
+          onTap: () {
+            Get.dialog(
+              AlertDialog(
+                title: Text('Excluir Produto'),
+                content:
+                    Text('Deseja realmente excluir o produto ${product.name}?'),
+                actions: <Widget>[
+                  FlatButton(
+                    onPressed: () {
+                      UploadService().removeImages(product.imagesUrl);
+                      ProductService().removeProduct(product);
+                      productController.reload();
+                      Get.close(1);
+                    },
+                    child: Text('Sim'),
+                    textColor: Theme.of(context).accentColor,
+                  ),
+                  FlatButton(
+                    onPressed: () => Get.close(1),
+                    child: Text('Não'),
+                    textColor: Theme.of(context).accentColor,
+                  ),
+                ],
+              ),
+            );
+          },
+          leading: Icon(
+            Icons.close,
+            color: Theme.of(context).errorColor,
+          ),
+          title: Text(
+            'Excluir',
+            style: TextStyle(
+              color: Theme.of(context).errorColor,
+            ),
+          ),
+        ),
+      ],
       title: Text(
         product.name,
         textAlign: TextAlign.left,
         overflow: TextOverflow.ellipsis,
       ),
       subtitle: Text('R\$' + product.price.toStringAsFixed(2)),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.camera_alt,
-            ),
-            onPressed: () => Get.to(AddPhotos(product: product)),
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.edit,
-            ),
-            onPressed: () => Get.to(
-              AddProduct(product: this.product),
-            ),
-          ),
-          IconButton(
-            icon: Icon(
-              Icons.close,
-              color: Theme.of(context).errorColor,
-            ),
-            onPressed: () {
-              Get.dialog(
-                AlertDialog(
-                  title: Text('Exluir Produto'),
-                  content: Text(
-                      'Deseja realmente excluir o produto ${product.name}?'),
-                  actions: <Widget>[
-                    FlatButton(
-                      onPressed: () {
-                        UploadService().removeImages(product.imagesUrl);
-                        ProductService().removeProduct(product);
-                        productController.reload();
-                        Get.close(1);
-                      },
-                      child: Text('Sim'),
-                      textColor: Theme.of(context).accentColor,
-                    ),
-                    FlatButton(
-                      onPressed: () => Get.close(1),
-                      child: Text('Não'),
-                      textColor: Theme.of(context).accentColor,
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-        ],
-      ),
     );
   }
 }
